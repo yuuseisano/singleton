28a99f345db3116341829bdb8975d07d0833acbf
4D-20\koyo <a>
2026-07-07 13:22:02 +0900

    シングルトンの基本形

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿class GameManager {
+private:
+	GameManager() {}
+
+public:
+
+	GameManager(const GameManager&) = delete;
+	GameManager& operator=(const GameManager&) = delete;
+
+
+	static GameManager& Instance() {
+				static GameManager instance;
+				return instance;
+	}
+
+	void StartGame() {
+		// Start the game logic here
+	}
+};
+
+int main() {
+	GameManager::Instance().StartGame();
+}
