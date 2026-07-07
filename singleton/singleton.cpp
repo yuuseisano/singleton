fa4fe05dd8657bdf3a7768c3a11c48f9c87fcfd8
4D-20\koyo <a>
2026-07-07 14:07:22 +0900

    コメントの追加及び、コードの編集

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -1,23 +1,39 @@
-﻿class GameManager {
+﻿#include <iostream>
+
+class GameManager {
 private:
+	/// <summary>
+	/// オブジェクトを外部から生成するのを防ぐためのprivateコンストラクタ
+	/// </summary>
 	GameManager() {}
 
 public:
-
+	/// <summary>
+	/// 別のオブジェクトを禁止するためのコピーコンストラクタ
+	/// </summary>
+	/// <param name=""></param>
 	GameManager(const GameManager&) = delete;
 	GameManager& operator=(const GameManager&) = delete;
 
-
+	/// <summary>
+	/// スレッドセーフな、Gamemanagerの唯一のオブジェクトを返すことができる
+	/// </summary>
+	/// <returns></returns>
 	static GameManager& Instance() {
-				static GameManager instance;
+				static GameManager instance;	// C++11以降のスレッドセーフなローカルstatic
 				return instance;
 	}
 
 	void StartGame() {
 		// Start the game logic here
+		std::cout << "Game started!" << std::endl;
 	}
 };
 
 int main() {
 	GameManager::Instance().StartGame();
+
+	//GameManager::Instance() = GameManager::Instance();
+
+	//GameManager obj1;
 }
